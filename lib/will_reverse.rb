7f498a1c4de868073dc93241146a51390d671074
eldenhowes <williameldenhowes@gmail.com>
2019-09-25 08:37:17 +0100

    method handles arrays of a length of two correctly

--- lib/will_reverse.rb
+++ lib/will_reverse.rb
@@ -1,6 +1,10 @@
 class Array
 
   def will_reverse
+
+    if self.length > 1
+      self[0], self[1] = self[1], self[0]
+    end
     self
   end
 end
--- spec/will_reverse_spec.rb
+++ spec/will_reverse_spec.rb
@@ -4,4 +4,8 @@ describe '#will_reverse' do
   it 'will return the samem array if only 1 in length' do
     expect([1].will_reverse).to eq([1])
   end
+
+  it 'will return the reverse of an array of length of 2' do
+    expect([1,2].will_reverse).to eq([2,1])
+  end
 end
