94cba3bd4f34f90b7c5bb1638da90999befd000e
eldenhowes <williameldenhowes@gmail.com>
2019-09-25 08:41:56 +0100

    method correctly handles arrays with a length of 3

--- lib/will_reverse.rb
+++ lib/will_reverse.rb
@@ -3,7 +3,11 @@ class Array
   def will_reverse
 
     if self.length > 1
-      self[0], self[1] = self[1], self[0]
+      if self.length % 2 == 0
+        self[0], self[1] = self[1], self[0]
+      else
+      self[-1], self[0] = self[0], self[-1]
+      end
     end
     self
   end
--- spec/will_reverse_spec.rb
+++ spec/will_reverse_spec.rb
@@ -8,4 +8,8 @@ describe '#will_reverse' do
   it 'will return the reverse of an array of length of 2' do
     expect([1,2].will_reverse).to eq([2,1])
   end
+
+  it 'will return the reverse of an array of length of 3' do
+    expect([1,2,3].will_reverse).to eq([3,2,1])
+  end
 end
